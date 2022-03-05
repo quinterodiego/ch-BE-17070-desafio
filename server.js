006ad6b7ca47a05233afdb3e550a1d86b380414b
Diego Quintero <d86webs@gmail.com>
2022-03-05 17:52:49 -0300

    clase 14

--- server.js
+++ server.js
@@ -1,86 +1,36 @@
 const express = require('express');
 const server = express();
-const path = require('path')
 
-const { createServer } = require('http');
-const { Server } = require('socket.io');
-const Contenedor = require('./Contenedor');
-
-const { engine } = require('express-handlebars')
-
-const { getMessages, saveMessage } = require('./data/chats.js');
-
-const httpServer = createServer(server);
-const io = new Server(httpServer);
+const productosRouter = require('./routers/productos');
+const cargaRouter = require('./routers/charge');
+const cartRouter = require('./routers/cart');
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.use( express.static('public') );
+server.use('/carga', cargaRouter);
+server.use('/productos', productosRouter);
+server.use('/carrito', cartRouter);
 
-server.engine('handlebars', engine({
-  layoutsDir: path.join(__dirname, 'views/layouts')
-}))
-server.set('view engine', 'handlebars')
+server.set('view engine', 'ejs');
 
 const PORT = 8080;
-const productosContenedor = new Contenedor('./data/products.json');
 
-io.on('connection', async socket => {
-  console.log(`¡Nuevo cliente conectado! socketid: ${socket.id}`);
-  const products = await productosContenedor.getAll();
-  io.sockets.emit('products', products);
-
-  socket.on('new-product', async product => {
-    await productosContenedor.save(product);
-    products = await productosContenedor.getAll();
-    console.log('PRODUCTOS', products)
-    io.sockets.emit('products', products);
-  });
-
-  const messages = getMessages();
-  socket.emit('messages', messages);
-
-  socket.on('new-message', message => {
-    saveMessage(message);
-    const messages = getMessages();
-    io.sockets.emit('messages', messages);
-});
+server.get('/', (req, res) => {
+  res.render('pages/index');
 });
 
-server.get('/' , (req, res) => {
-  res.status(200).render('main', { layout: 'index' });
+server.get('/carga', (req, res) => {
+  res.render('pages/carga');
 });
 
-server.get('/lista-productos', async (req, res) => {
-  const lista = await productosContenedor.getAll();
-  res.render('../views/layouts/lista-productos', {
-    message: 'success',
-    data: lista
-  });
-})
-
-server.post('/api/products', async (req, res) => {
-  
-  const nuevoProducto = req.body;
-
-  const idProductoGuardado = await productosContenedor.save(nuevoProducto);
-
-    res.redirect('/lista-productos')
-
-    res.send({
-      message: 'Producto guardado',
-      data: {
-        ...nuevoProducto,
-        id: idProductoGuardado
-      }
-  });
-
-})
+server.get('/productos', (req, res) => {
+  res.render('pages/productos');
+});
 
 
-const http = httpServer.listen(PORT, () => 
-    console.log(`Servidor abierto en http://localhost:${PORT}/`)
-)
+server.get('/carrito', (req, res) => {
+  res.render('pages/carrito');
+});
 
-http.on('error', error => console.log('Error en servidor:', error));
+server.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
